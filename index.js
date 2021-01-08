ebc72233b11d356678497ca81b1bd85528942c3c
Fox1210 <pablogomezmelendez@gmail.com>
2021-01-08 14:23:19 +0100

    Modificación del fichero suprimiendo la sacada de datos del formulario

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ import "./style.css";
 //declaramos las variables
 var numA;
 var numB;
-var prueba;
-//sacamos los valores del formulario
-numA = document.getElementById("numero1").value;
-numB = document.getElementById("numero2").value;
+// var prueba;
+// //hay un error uno saca los valores del formulario
+// //sacamos los valores del formulario
+// numA = document.getElementsByName("numero1")[0].values;
+// numB = document.getElementById("numero2").values;
 
-prueba = "texto de prueba: a= " + numA + ", el numero b= " + numB;
-console.log(prueba);
+// prueba = "texto de prueba: a= " + numA + ", el numero b= " + numB;
+// console.log(prueba);
